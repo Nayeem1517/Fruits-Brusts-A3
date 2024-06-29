64d30cd9cdfb66a471c3b8d55053b5e81fb88c7e
Nayeem <mahfujurrahamannayeem@gmail.com>
2024-06-29 23:43:27 +0600

    Completing The Assingment

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// Linking Tailwind Intellience
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
